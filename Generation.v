cb3bc684889d15e0425629312cb728e11558b4ee
Rohith Asrk <rohith.asrk@gmail.com>
2017-05-01 12:09:40 +0530

    Fixed simulation and algorithm errors

--- Generation.v
+++ Generation.v
@@ -18,6 +18,7 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
 module FF(clk,D,Q,Qbar);
 input clk,D;
 output reg Q;
@@ -38,17 +39,14 @@ endmodule
 
 module Generation(
 	input clk,start,WM_select,
-	//input [7:0]key,
-	output /*reg*/ [1:0] WM_Data
+	output [1:0] WM_Data
 );
 
 wire [7:0] Q, Qbar;
 wire [7:0] D;
 assign WM_Data[0] = Q[0];
 assign WM_Data[1] = (WM_select) ? Q[1]^Q[0]: 1'b0;
-parameter key = 8'b01101010;//subject to change
-
-//wire A,B,C;	
+parameter key = 8'b01101010; //subject to change
 
 FF #(key[0]) FF0(clk,D[0],Q[0],Qbar[0]);
 FF #(key[1]) FF1(clk,D[1],Q[1],Qbar[1]);
@@ -59,23 +57,6 @@ FF #(key[5]) FF5(clk,D[5],Q[5],Qbar[5]);
 FF #(key[6]) FF6(clk,D[6],Q[6],Qbar[6]);
 FF #(key[7]) FF7(clk,D[7],Q[7],Qbar[7]);
 
-//initial
-//begin 
-//Q=8'b00000000;
-
-
-/*wire [7:0] temp = D;
-
-xor X1(temp[2],Q[1],D[0]);
-xor X2(temp[3],Q[2],D[0]);
-xor X3(temp[4],Q[3],D[0]);
-or O1(B,temp[4],temp[5],temp[6]);
-or O2(C,temp[1],temp[2],temp[3]);
-xor X4(A,B,C);
-xor X5(temp[0],Q[7],A);
-*/ 
-
-
 assign D[0]=Q[7]^(~((D[4]|D[5]|D[6]|D[7])|(D[1]|D[2]|D[3])));
 assign D[1]=Q[0];
 assign D[2]=Q[1]^D[0];
@@ -84,9 +65,5 @@ assign D[4]=Q[3]^D[0];
 assign D[5]=Q[4];
 assign D[6]=Q[5];
 assign D[7]=Q[6];
-
-//Q[4]=Q[3]^D[0];
-//
-
 
 endmodule
--- Insertion.v
+++ Insertion.v
@@ -18,6 +18,7 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
 module Insertion(
 	input clk,
 	input start,
@@ -25,28 +26,32 @@ module Insertion(
 	input [7:0]Data2,
 	input [7:0]Data3,
 	input [7:0]Data4,
-	input [7:0]a1,
-	input [7:0]a2,
+	//input [7:0]a1,
+	//input [7:0]a2,
 	input [1:0]WM_data,
 	output [7:0]WM_IM_Data
 );
 
 wire [8:0] Adder1, Adder2, Adder;
-wire [7:0] shiftedAdder1 = Adder1[8:1];
-wire [7:0] shiftedAdder2 = Adder2[8:1];
+
+
 wire [7:0] m1, m11, m2, m12;
 reg [7:0] temp;
+
+adder8 A1(.a(Data2), .b(Data4), .add(1), .s(Adder1));
+wire [7:0] shiftedAdder1 = Adder1[8:1];
+
+adder8 A2(.a(Data3), .b(shiftedAdder1), .add(1), .s(Adder2));
+wire [7:0] shiftedAdder2 = Adder2[8:1];
 
 Amul mul2(.a(shiftedAdder2), .clk(clk), .p(m2));
 
 Cmul mul1(.a(Data1), .p(m11));
-Bmul mul3(.a(Data2), .p(m12));
+Bmul mul3(.a(Data1), .p(m12));
 
 assign m1 = (WM_data == 2'b01)? m11 : ((WM_data == 2'b10)? m12 : 0);
 
-adder8 A1(.a(Data1), .b(Data2), .add(1), .s(Adder1));
-adder8 A2(.a(Data3), .b(shiftedAdder1), .add(1), .s(Adder2));
 adder8 A3(.a(m1), .b(m2), .add(1), .s(Adder));
 
-assign WM_IM_Data = (WM_data== 2'b00)? Data1 : Adder ;
+assign WM_IM_Data = (WM_data== 2'b00 | WM_data == 2'b11)? Data1 : Adder[7:0];
 endmodule
